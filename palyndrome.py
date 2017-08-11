716ff0d7e756fca70e353603ac0d6151340b5e3d
newic47 <lameri@ouvaton.org>
2017-08-11 19:42:33 +0200

    lehlo world

--- palyndrome.py
+++
@@ -1,8 +0,0 @@
-import getpass
-
-def crypt(e):
-    """mais qu'est ce que c'est que cà michael ?? """
-    i = lambda e: e[len(e)/2::-1]+e[len(e)/2+1::1]
-    return i(e)
-
-motdepasse = lambda f: crypt(getpass))
